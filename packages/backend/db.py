a3af0d37d5ac97bc9250b612742068e4ba519b93
Jack Lee <welsar55@gmail.com>
2023-04-22 03:09:50 -0500

    🥩 chatgpt can write all the hard sql it turns out

--- packages/backend/db.py
+++ packages/backend/db.py
@@ -16,18 +16,23 @@ def update_geom(conn, lat, lon, userid, groupid):
     update_geom_sql = """
         WITH input AS (
             SELECT ST_MakePoint(%s, %s) AS center
+        ), upsert AS (
+            INSERT INTO geometries (userid, groupid, geometry)
+            VALUES (%s, %s, ST_Buffer((SELECT center FROM input), 0.1, 'quad_segs=2'))
+            ON CONFLICT (userid, groupid)
+            DO UPDATE SET geometry = ST_Union((
+                SELECT geometry
+                FROM geometries
+                WHERE userid = %s AND groupid = %s
+            ), ST_Buffer((SELECT center  FROM input), 0.1, 'quad_segs=2'))
+            RETURNING geometry
         )
-        INSERT INTO geometries (userid, groupid, geometry)
-        VALUES (%s, %s, ST_Buffer((SELECT center FROM input), 0.1, 'quad_segs=2'))
-        ON CONFLICT (userid, groupid)
-        DO UPDATE SET geometry = ST_Union((
-            SELECT geometry
-            FROM geometries
-            WHERE userid = %s AND groupid = %s
-        ), ST_Buffer((SELECT center  FROM input), 0.1, 'quad_segs=2'));
-
+        UPDATE geometries g
+        SET geometry = ST_Difference(g.geometry, u.geometry)
+        FROM upsert u
+        WHERE g.groupid = %s AND g.userid != %s;
     """
     
     with conn.cursor() as cur:
-        cur.execute(update_geom_sql, (lat, lon, userid, groupid, userid, groupid))
+        cur.execute(update_geom_sql, (lat, lon, userid, groupid, userid, groupid, groupid, userid))
         conn.commit()
