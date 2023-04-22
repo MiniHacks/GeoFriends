dd69c1d776e8e021fe553b5c680b2e33e6674037
cbbor <c.b.borisova@gmail.com>
2023-04-22 02:17:37 -0700

    � yay firebase

--- packages/mobile/screens/HomeScreen.js
+++ packages/mobile/screens/HomeScreen.js
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, ImageBackground, Button, StyleSheet, TouchableOpacity} from "react-native";
+import {
+  Text,
+  View,
+  ImageBackground,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import firestore from "@react-native-firebase/firestore";
 import SignUp from "./SignUp";
-import { Button, SafeAreaView, ScrollView, Text, View } from "react-native";
+import { SafeAreaView, ScrollView } from "react-native";
 import {
   GoogleSignin,
   GoogleSigninButton,
@@ -18,7 +25,6 @@ GoogleSignin.configure({
 //const usersCollection = firestore().collection("users");
 
 export default function HomeScreen() {
-
   /*Google Authentication*/
   const [loggedIn, setloggedIn] = useState(false);
   const [userInfo, setuserInfo] = useState([]);
@@ -54,7 +60,7 @@ export default function HomeScreen() {
       console.error(error);
     }
   };
-  
+
   useEffect(() => {
     GoogleSignin.configure({
       scopes: ["email"], // what API you want to access on behalf of the user, default is email and profile
@@ -65,38 +71,65 @@ export default function HomeScreen() {
   }, []);
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    // style={{
+    //   flex: 1,
+    //   justifyContent: "center",
+    //   alignItems: "center",
+    // }}
     >
       {/* import the background image from assets */}
-      <ImageBackground source={require("../assets/homescreen_background.png")} style={{flex: 1, width: "100%", height: "100%",
-        position: "absolute" }}/>
+      <ImageBackground
+        source={require("../assets/homescreen_background.png")}
+        // style={{ flex: 1, width: "100%", height: "100%", position: "absolute" }}
+      />
 
       {/*Add centered sign in text using raleway font using size 35, black color*/}
 
-
       {/*Create a touchableOpacity button with a text inside with rounded corners that uses the sign_in_button.png*/}
-      <TouchableOpacity style={styles.button} onPress={() => console.log("pressed")}>
-        <ImageBackground source={require("../assets/sign_in_button.png")} style={{flex: 1, width: "100%", height: "100%",
-          position: "absolute" }}/>
+      <TouchableOpacity
+        // style={styles.button}
+        onPress={() => console.log("pressed")}
+      >
+        <ImageBackground
+          source={require("../assets/sign_in_button.png")}
+          // style={{
+          //   flex: 1,
+          //   width: "100%",
+          //   height: "100%",
+          //   position: "absolute",
+          // }}
+        />
       </TouchableOpacity>
       {/*Add text that says if you don't have an account sign up, with the sign up being a hyperlink and they
        are on the same line*/}
-      <Text style={{fontFamily: "Raleway", fontSize: 15, color: "black", marginTop: 10}}>Don't have an account? </Text>
+      <Text
+      // style={{
+      //   fontFamily: "Raleway",
+      //   fontSize: 15,
+      //   color: "black",
+      //   marginTop: 10,
+      // }}
+      >
+        Don't have an account?{" "}
+      </Text>
       <TouchableOpacity onPress={() => console.log("pressed")}>
         {/* add underline */}
-        <Text style={{fontFamily: "Raleway", fontSize: 15, color: "black", marginTop: 10, textDecorationLine: 'underline'}}>sign up</Text>
+        <Text
+          style={{
+            fontFamily: "Raleway",
+            fontSize: 15,
+            color: "black",
+            marginTop: 10,
+            textDecorationLine: "underline",
+          }}
+        >
+          sign up
+        </Text>
       </TouchableOpacity>
 
       {/*Add a button that will take the user to the SignUp screen */}
 
-
-      <Text>{user?.name}</Text>
-      
-            <SafeAreaView>
+      <SafeAreaView>
         <ScrollView contentInsetAdjustmentBehavior="automatic">
           <View>
             <View>
