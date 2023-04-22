9f701c90913bc4deab00330ee194ac06eedef045
Julia Truong <hello@juliatru.ong>
2023-04-22 14:44:48 -0700

    😭Draw polygons from geojson + Zoomin/Zoomout on map

--- packages/mobile/components/Overlay.js
+++ packages/mobile/components/Overlay.js
@@ -1,13 +1,15 @@
-import MapView, {Circle, Polyline, Polygon, PROVIDER_GOOGLE, Marker } from "react-native-maps";
-import { View, StyleSheet, Dimensions, PanResponder, Animated } from "react-native";
-import { PinchGestureHandler, State } from 'react-native-gesture-handler';
-import React, { useState, useRef } from "react"; // remove PROVIDER_GOOGLE import if not using Google Maps
+import MapView, {Geojson, PROVIDER_GOOGLE} from "react-native-maps";
+import {Dimensions, StyleSheet, View} from "react-native";
+import {State} from 'react-native-gesture-handler';
+import React, {useEffect, useRef, useState} from "react"; // remove PROVIDER_GOOGLE import if not using Google Maps
 
-const { width, height } = Dimensions.get('window');
+const SAMPLE_DATA = {"welsar55":{"type":"MultiPolygon","coordinates":[[[[0.9,-3.231089149e-16],[0.929289322,0.070710678],[1,0.1],[1.070710678,0.070710678],[1.1,0],[1.070710678,-0.070710678],[1,-0.1],[0.929289322,-0.070710678],[0.9,-3.231089149e-16]]],[[[2.929289322,0.070710678],[3,0.1],[3.025,0.089644661],[3.05,0.1],[3.120710678,0.070710678],[3.15,0],[3.120710678,-0.070710678],[3.05,-0.1],[3.025,-0.089644661],[3,-0.1],[2.929289322,-0.070710678],[2.9,-3.231089149e-16],[2.929289322,0.070710678]]],[[[2,-0.1],[1.961731657,-0.092387953],[1.929289322,-0.070710678],[1.907612047,-0.038268343],[1.9,-3.231089149e-16],[1.907612047,0.038268343],[1.929289322,0.070710678],[1.961731657,0.092387953],[2,0.1],[2.033778833,0.093280972],[1.979289322,0.070710678],[1.95,-3.231089149e-16],[1.979289322,-0.070710678],[2.033778833,-0.093280972],[2,-0.1]]],[[[2.1,0.1],[2.170710678,0.070710678],[2.2,0],[2.170710678,-0.070710678],[2.1,-0.1],[2.075,-0.089644661],[2.120710678,-0.070710678],[2.15,0],[2.120710678,0.070710678],[2.075,0.089644661],[2.1,0.1]]]]},"welsar56":{"type":"Polygon","coordinates":[[[2.15,0],[2.120710678,-0.070710678],[2.05,-0.1],[1.979289322,-0.070710678],[1.95,-3.231089149e-16],[1.979289322,0.070710678],[2.05,0.1],[2.120710678,0.070710678],[2.15,0]]]}}
+
+const {width, height} = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
-const LATITUDE = 37.78825;
-const LONGITUDE = -122.4324;
-const LATITUDE_DELTA = 0.0922;
+const LATITUDE = 34.052235;
+const LONGITUDE = -118.243683;
+const LATITUDE_DELTA = 0.0122;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 const SPACE = 0.01;
 
@@ -18,8 +20,7 @@ const styles = StyleSheet.create({
     width: Dimensions.get("window").width,
     justifyContent: "flex-end",
     alignItems: "center",
-  },
-  map: {
+  }, map: {
     ...StyleSheet.absoluteFillObject,
   },
 });
@@ -28,32 +29,26 @@ const styles = StyleSheet.create({
 const Map = () => {
   const mapRef = useRef(null);
 
+
   const [circle, setCircle] = useState({
     center: {
-      latitude: LATITUDE + SPACE,
-      longitude: LONGITUDE + SPACE,
-    },
-    radius: 700,
+      latitude: LATITUDE + SPACE, longitude: LONGITUDE + SPACE,
+    }, radius: 700,
   });
 
   const [region, setRegion] = useState({
-    latitude: LATITUDE,
-    longitude: LONGITUDE,
-    latitudeDelta: LATITUDE_DELTA,
-    longitudeDelta: LONGITUDE_DELTA,
+    latitude: LATITUDE, longitude: LONGITUDE, latitudeDelta: LATITUDE_DELTA, longitudeDelta: LONGITUDE_DELTA,
   });
 
 
   const onPinchEvent = event => {
     if (event.nativeEvent.state === State.ACTIVE) {
-      const { scale } = event.nativeEvent;
+      const {scale} = event.nativeEvent;
       const newLatitudeDelta = region.latitudeDelta / scale;
       const newLongitudeDelta = region.longitudeDelta / scale;
 
       setRegion({
-        ...region,
-        latitudeDelta: newLatitudeDelta,
-        longitudeDelta: newLongitudeDelta,
+        ...region, latitudeDelta: newLatitudeDelta, longitudeDelta: newLongitudeDelta,
       });
     }
   };
@@ -61,46 +56,56 @@ const Map = () => {
   const onPanResponderMove = (event, gestureState) => {
     const dx = -gestureState.dx;
     const dy = -gestureState.dy;
-    const { latitude, longitude, latitudeDelta, longitudeDelta } = region;
+    const {latitude, longitude, latitudeDelta, longitudeDelta} = region;
     const lat = latitude - (dy * latitudeDelta * 2) / 667;
     const lng = longitude + (dx * longitudeDelta * 2) / 375;
-    setRegion({ ...region, latitude: lat, longitude: lng });
+    setRegion({...region, latitude: lat, longitude: lng});
     mapRef.current.animateToRegion(region, 0);
   };
 
-  const panResponder = PanResponder.create({
-    onStartShouldSetPanResponder: () => true,
-    onPanResponderMove,
+
+  const GROUP = "welsar-friends";
+  const [geostate, setGeostate] = useState(SAMPLE_DATA);
+
+  useEffect(() => {
+    fetch("http://172.190.74.123:8000/get_group_geom/" + GROUP).then((response) => {
+      response.json().then((data) => {
+        //   iterate through every object in data
+        setGeostate(data);
+      });
+    });
+  }, []);
+
+  const template = shit => ({
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": shit
+      }
+    ]
+  })
+
+
+  const polygons = geostate && Object.entries(geostate).map(([user, geometry]) => {
+    return (<Geojson
+      key={user}
+      strokeColor="red"
+      fillColor="green"
+      strokeWidth={2}
+      geojson={template(geometry)}
+    />);
   });
 
   return (
 
-    <PinchGestureHandler onGestureEvent={onPinchEvent}>
-      <View style={styles.container} {...panResponder.panHandlers}>
-        <MapView style={styles.map} ref={mapRef} initialRegion={region}
-          provider={PROVIDER_GOOGLE}>
-          <Polygon
-            coordinates={[
-              { latitude: 37.8025259, longitude: -122.4351431 },
-              { latitude: 37.7896386, longitude: -122.421646 },
-              { latitude: 37.7665248, longitude: -122.4161628 },
-              { latitude: 37.7734153, longitude: -122.4577787 },
-              { latitude: 37.7948605, longitude: -122.4596065 },
-              { latitude: 37.8025259, longitude: -122.4351431 },
-            ]}
-          />
-          <Circle
-            center={circle.center}
-            radius={circle.radius}
-            fillColor="rgba(255, 255, 255, 1)"
-            strokeColor="rgba(0,0,0,0.5)"
-            zIndex={2}
-            strokeWidth={2}
-          />
-        </MapView>
-      </View>
-    </PinchGestureHandler>
-  );
+    <View style={styles.container}>
+      <MapView style={styles.map} ref={mapRef} initialRegion={region}
+               provider={PROVIDER_GOOGLE}>
+        {polygons}
+      </MapView>
+    </View>);
 };
 
 export default Map;
