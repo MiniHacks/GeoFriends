7962541c82a67fe00b8dcf38a8cad8df719d2c28
cbbor <c.b.borisova@gmail.com>
2023-04-22 01:20:56 -0500

    � lmost working

--- packages/mobile/screens/HomeScreen.js
+++ packages/mobile/screens/HomeScreen.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import firestore from "@react-native-firebase/firestore";
+
+//const usersCollection = firestore().collection("users");
 
 export default function HomeScreen() {
+  const userId = "bMacFrTg7HGIQoaKAilu";
+  const [user, setUser] = useState(null);
+  function User({ userId }) {
+    useEffect(() => {
+      const subscriber = firestore()
+        .collection("users")
+        .doc(userId)
+        .onSnapshot((documentSnapshot) => {
+          console.log("User data: ", documentSnapshot.data());
+          setUser(documentSnapshot.data());
+        });
+
+      // Stop listening for updates when no longer required
+      return () => subscriber();
+    }, [userId]);
+  }
+
   return (
     <View
       style={{
@@ -10,7 +30,8 @@ export default function HomeScreen() {
         alignItems: "center",
       }}
     >
-      <Text>Home</Text>
+      <Text>Hello World!</Text>
+      <Text>{user?.name}</Text>
     </View>
   );
 }
